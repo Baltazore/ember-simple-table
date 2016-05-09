b7e3f46a17be3e98fb41a00d2daafea139523610
Kirill Shaplyko <k.shaplyko@gmail.com>
2016-05-09 13:54:49 +0300

    Add missing tests for simple-table-cell

--- tests/integration/components/simple-table-cell-test.js
+++ tests/integration/components/simple-table-cell-test.js
@@ -5,17 +5,47 @@ moduleForComponent('simple-table-cell', 'Integration | Component | simple table 
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });"
+test('it renders with sorting arrow', function(assert) {
+  this.set('column', { items: { key: 'baz', name: "Baz" } });
+  this.set('sorting', { key: 'baz', order: 'asc'});
 
-  this.set('dataKey', 'foo');
-  this.set('row', { foo: 'Foo' });
-  this.on('addCell', () => true);
+  this.render(hbs`
+    {{simple-table-cell columns=column sortingCriteria=sorting }}
+  `);
 
-  this.render(hbs`{{simple-table-cell row=row addCell=(action 'addCell') dataKey=dataKey}}`);
+  assert.equal(this.$('.sortable').length, 1, 'Creates sortable class cell');
+  assert.equal(this.$('th').length, 1, 'Creates th cell');
+  assert.equal(this.$('i.sort-arrow').length, 1, 'Creates sorting arrow cell');
+  assert.equal(this.$('th span').text().trim(), 'Baz');
+});
 
-  assert.equal(this.$().text().trim(), 'Foo');
-  assert.equal(this.$('th').length, 1, 'Creates 1 cell');
-  assert.equal(this.$('.sortable').length, 1, 'Add .sortable class to cell');
+test('it renders without sorting arrow', function(assert) {
+  this.set('column', { items: { key: 'baz', name: "Baz", sortable: false } });
+  this.set('sorting', { key: 'baz', order: 'asc'});
+
+  this.render(hbs`
+    {{simple-table-cell columns=column sortingCriteria=sorting }}
+  `);
+
+  assert.equal(this.$('.sortable').length, 0, 'Creates sortable class cell');
+  assert.equal(this.$('th').length, 1, 'Creates th cell');
+  assert.equal(this.$('i').length, 0, 'Not creates sorting arrow cell');
+  assert.equal(this.$('th span').text().trim(), 'Baz');
+});
+
+test('it renders 2 spans for complex header cell', function(assert) {
+  this.set('column', { hasMultipleColumns:true, items: [
+    { key: 'baz', name: "Baz", sortable: false },
+    { key: 'foo', name: "Foo" }
+  ] });
+  this.set('sorting', { key: 'baz', order: 'asc'});
+
+  this.render(hbs`
+    {{simple-table-cell columns=column sortingCriteria=sorting }}
+  `);
+
+  assert.equal(this.$('th').length, 1, 'Creates th cell');
+  assert.equal(this.$('th span').length, 2, 'Creates 2 span in 1 cell');
+  assert.equal(this.$('th span:first').text().trim(), 'Baz');
+  assert.equal(this.$('th span:last').text().trim(), 'Foo');
 });
